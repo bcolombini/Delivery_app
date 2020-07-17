9c7d163cf9811a08db3366750b477ce896a7f9e0
Bruno Colombini <bruno.colombini@me.com>
2020-07-17 00:52:06 -0300

    Local Storage

--- src/app/address/address.class.ts
+++ src/app/address/address.class.ts
@@ -1,6 +1,7 @@
 import { NavController } from '@ionic/angular';
 import { AddressService } from './address.service';
 import { Storage } from '@ionic/storage';
+import { Address } from '../models/address.model';
 
 export abstract class AddressClass {
 
@@ -18,8 +19,17 @@ export abstract class AddressClass {
     }
 
     public async saveAddress(){
-        this.storage.set("Address","Rua")
-        console.log(await this.storage.get("Address"))
+        let a = new Address()
+        a.city = "RiO"
+        a.complement = "apto 102"
+        a.neighborhood = "Centro"
+        a.number = "61"
+        a.state = "RJ"
+        a.zipcode = "20200000"
+        a.nick = "Minha casa"
+        this.storage.set("LIST_ADDRESS",JSON.stringify(a))
+        console.log(await this.storage.get("LIST_ADDRESS"))
+        console.log(JSON.parse(await this.storage.get("LIST_ADDRESS")))
         this.navController.back()
     }
 
---
+++ src/app/models/address.model.ts
@@ -0,0 +1,10 @@
+export class Address{
+    nick?:string;
+    street:string;
+    number:string;
+    complement:string;
+    neighborhood:string;
+    city:string;
+    state:string;
+    zipcode:string
+}
