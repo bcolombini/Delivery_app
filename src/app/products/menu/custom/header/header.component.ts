74c0f70401d86ec1edbfb463aabdd12814090181
Bruno Colombini <bruno.colombini@me.com>
2020-07-22 02:12:30 -0300

    Test subscribe open time

--- src/app/products/menu/custom/header/header.component.ts
+++ src/app/products/menu/custom/header/header.component.ts
@@ -1,23 +1,39 @@
-import {Component, OnInit, Output, EventEmitter, ViewChild, AfterViewInit, ElementRef, Input} from '@angular/core';
-import { IonImg } from '@ionic/angular';
+  import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ViewChild,
+  AfterViewInit,
+  ElementRef,
+  Input,
+  AfterContentInit
+} from '@angular/core';
+import {IonImg, ViewDidEnter, ViewWillEnter} from '@ionic/angular';
 import {Information} from "../../../../models/information.model";
+import {InformationService} from "../../../../service/information.service";
 
 @Component({
   selector: 'custom-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements AfterContentInit{
 
   @ViewChild("ionImgFinal") ionImage: ElementRef
   @Input("information") information: Information
 
-  constructor() { }
+  public isOpen = true
 
-  ngOnInit() {}
+  constructor(private informationService:InformationService) {
+    this.informationService.isOpenEstablishmentSubject()
+  }
+
+  async ngAfterContentInit() {
+    this.informationService.getEstablishmentSubject().subscribe(value => value.then(x=>this.isOpen = x))
+  }
 
   public loadImageError(){
-   
     console.log('ERROR')
   }
   public didLoadImage(){
--- src/app/service/information.service.ts
+++ src/app/service/information.service.ts
@@ -4,6 +4,8 @@ import {Storage} from "@ionic/storage";
 import {Information} from "../models/information.model";
 import {DBConstants} from "../constants/DBConstants";
 import {URLConstants} from "../constants/URLConstants";
+import {Subject, interval, Observable} from "rxjs";
+import {throttle} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -11,19 +13,44 @@ import {URLConstants} from "../constants/URLConstants";
 export class InformationService {
 
   public information
+  public establishmentSubject:Subject<Promise<boolean>> = new Subject<Promise<boolean>>()
 
   constructor(
       private httpService:HttpService,
       private storage:Storage) { }
 
-  public async getInformation() {
+  public async getInformation():Promise<Information> {
     if(await this.isRecentVerification()){
       return await this.getInformationLocal()
     }
     const info = await this.getInformationFromWeb()
     await this.saveInformationLocal(info)
     return info
+  }
 
+  private async isOpenEstablishment(): Promise<boolean> {
+    this.information = await this.getInformationLocal()
+    let workOpen = this.information.today.work_open
+    let workClose = this.information.today.work_close
+    const date = new Date()
+    let currentHours = date.getHours().toString();
+    currentHours = ("0" + currentHours).slice(-2);
+    const dateNow = currentHours + ":" + date.getMinutes()
+    if ((workClose > workOpen && workClose < dateNow) ||
+        (workOpen >= dateNow && dateNow > workClose)) {
+      return false;
+    } else if (dateNow >= workOpen) {
+      return true;
+    }
+  }
+  
+  public getEstablishmentSubject():Observable<Promise<boolean>>{
+    return this.establishmentSubject
+  }
+
+  public isOpenEstablishmentSubject(){
+    interval(1000)
+        .subscribe(value => this.establishmentSubject.next(this.isOpenEstablishment()))
   }
 
   private async saveInformationLocal(info:Information){
@@ -38,7 +65,6 @@ export class InformationService {
 
   private async isRecentVerification(){
     let lastInfoUpdate = await this.storage.get(DBConstants.INFO_LAST_UPDATE)
-    console.log(lastInfoUpdate)
     if(Date.now()/1000 > lastInfoUpdate+86400){
       return false
     }
@@ -46,7 +72,6 @@ export class InformationService {
   }
 
   private async getInformationFromWeb(){
-    console.log("GET FROM WEB")
     return await this.httpService.getRequest(URLConstants.INFORMATION).toPromise() as Information
   }
 }
