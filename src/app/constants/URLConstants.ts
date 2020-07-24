f5aa7fe749c984976a30fb4d18bf8e050e690186
Bruno Colombini <bruno.colombini@me.com>
2020-07-24 01:31:05 -0300

    Control time

--- src/app/constants/URLConstants.ts
+++ src/app/constants/URLConstants.ts
@@ -1,7 +1,8 @@
 export const URLConstants = {
+    ERROR:"https://run.mocky.io/v3/802db918-f6c8-4375-9aec-fb5d772cc8bc",
     MENU:"https://run.mocky.io/v3/92cd4e1d-4a33-46f1-a82a-6b5633585574",
     INFORMATION:"https://run.mocky.io/v3/516abc80-e82d-429b-9938-06da38ec8017",
-    INFORMATION_FULL_TIME:"https://run.mocky.io/v3/6987a33a-a8c4-4532-8c3f-3f40d2c99556",
-    PAST_ORDERS:"https://run.mocky.io/v3/8eb73674-8821-4f3a-b3bc-a772554e6f2c",
+    INFORMATION_FULL_TIME:"https://run.mocky.io/v3/fdde3972-9254-41ce-98f2-37aa00c22326",
+    PAST_ORDERS:"https://run.mocky.io/v3/6d2589e5-a576-4c0f-9ab7-59b9db6d28c0",
     PROFILE:""
 }
--- src/app/past-orders/past-orders.component.ts
+++ src/app/past-orders/past-orders.component.ts
@@ -1,7 +1,7 @@
 import {AfterViewInit, Component} from '@angular/core';
 import {PastOrdersService} from "./past-orders.service";
 import {PastOrders} from "../models/order.model";
-import {LoadingController} from "@ionic/angular";
+import {AlertController, LoadingController, ViewDidEnter} from "@ionic/angular";
 import {TextConstants} from "../constants/TextConstants";
 
 @Component({
@@ -9,20 +9,30 @@ import {TextConstants} from "../constants/TextConstants";
   templateUrl: './past-orders.component.html',
   styleUrls: ['./past-orders.component.scss'],
 })
-export class PastOrdersComponent implements AfterViewInit {
+export class PastOrdersComponent implements ViewDidEnter {
 
   public pastOrders:PastOrders[] = []
 
   constructor(
       private pastOrdersService:PastOrdersService,
-      private loadingController:LoadingController) { }
+      private loadingController:LoadingController,
+      private alertController:AlertController) { }
 
-  async ngAfterViewInit() {
+  async ionViewDidEnter(){
     const loading = await this.loadingController.create({message:TextConstants.LOADING})
     await loading.present()
-    this.pastOrders = await this.pastOrdersService.getPastOrder()
+    try{
+      this.pastOrders = await this.pastOrdersService.getPastOrder()
+    } catch {
+      console.log("ERROR")
+      const alert = await this.alertController.create({
+        header: TextConstants.WARNING,
+        message: TextConstants.ERROR_HAPPEN,
+        buttons: [TextConstants.CLOSE]
+      });
+      await alert.present();
+    }
     await loading.dismiss()
-
   }
 
 
